4e9e2b657beb9030da5493bcfbe68c34ea9047e7
LiuYuWei <727826421@qq.com>
2021-01-25 10:53:50 +0800

    1

---
+++ springcloud/src/test/java/com/example/springcloud/SpringcloudApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.springcloud;
+
+
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
